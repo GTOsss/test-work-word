abda493e2d820c75466938ef84172212514ff6e8
Timothy Goncharov <sedmoy.mm@mail.ru>
2018-01-29 16:14:31 +0300

    Fix containers.

--- src/containers/navbar-home/navbar-home.js
+++ src/containers/navbar-home/navbar-home.js
@@ -4,7 +4,7 @@ import { NavBar, NavLink } from 'components/navbar';
 const NavbarHome = () => (
   <NavBar>
     <NavLink exact to="/">Главная</NavLink>
-    <NavLink to="/vocabularies">Словари</NavLink>
+    <NavLink exact to="/vocabularies">Словари</NavLink>
   </NavBar>
 );
 
--- src/containers/vocabulary-edit/vocabulary-edit.js
+++ src/containers/vocabulary-edit/vocabulary-edit.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Words from 'containers/forms/words';
-import WrapBodyScroll from 'components/wrap-body';
+import WrapBody from 'components/wrap-body';
 
 const VocabularyEdit = ({ match }) => (
-  <WrapBodyScroll scroll>
+  <WrapBody scroll>
     <Words id={match.params.id} />
-  </WrapBodyScroll>
+  </WrapBody>
 );
 
 VocabularyEdit.propTypes = {
